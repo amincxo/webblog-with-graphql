692e6f0347bc8ee5ee4b04124d42ea12199b531a
unknown <amincxo@gmail.com>
2024-10-11 03:00:34 +0330

    create authors page

--- src/components/author/Authors.js
+++ src/components/author/Authors.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import { useQuery } from '@apollo/client'
 import { GET_AUTHORS_INFO } from '../../graphql/queries'
+import { Avatar, Divider, Grid, Typography } from '@mui/material';
 
 
 function Authors() {
@@ -11,9 +12,27 @@ function Authors() {
 
     if(error) return <h3> متاسفانه زمان گرفتن اطلاعات به مشکل خوردیم </h3>
 
-    console.log(data)
+    const {authors} = data;
   return (
-    <div>Authors</div>
+    <Grid container sx={{boxShadow: "rgba(0,0,0,0.1) 0px 4px 12px" , borderRadius: 4 }} >
+        {authors.map((author, index) => (
+            <React.Fragment key={author.id} >
+            <Grid item xs={12} padding={2}  >
+                <a href={`/authors/${author.slug}`} style={{display:"flex", alignItems:"center" , textDecoration:"none" }} >
+                    <Avatar src={author.avatar.url}  sx={{margin:2}}/>
+                    <Typography component="p" variant='p' color='text.secondary'  >
+                    {author.name}
+                    </Typography>
+                </a>
+            </Grid>
+            {index !== authors.length -1 && (
+            <Grid item xs={12} >
+                <Divider variant='middle' />
+            </Grid>
+            )}
+            </React.Fragment>
+        ) )}
+    </Grid>
   )
 }
 
