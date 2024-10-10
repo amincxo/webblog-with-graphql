fbb93d671b628bb76dbb300376c7504aa346ac3c
unknown <amincxo@gmail.com>
2024-10-11 02:32:15 +0330

    card style complate matrial styles and link form react router dom

--- src/components/shared/CardEl.js
+++ src/components/shared/CardEl.js
@@ -1,9 +1,30 @@
+import { Avatar, Button, Card, CardActions, CardContent, CardHeader, CardMedia, Divider, Typography } from '@mui/material'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-function CardEl() {
+function CardEl({title , slug , coverPhoto , author}) {
   return (
-    <div>CardEl</div>
+    <Card sx={{boxShadow: "rgba(0,0,0,0.1) 0px 4px 12px ", borderRadius: 4}} >
+        <CardHeader 
+        avatar={<Avatar src={author.avatar.url} sx={{marginLeft: 2}}/>} 
+        title={
+        <Typography component="p" variant="p" color="text.secondary" >
+            {author.name}</Typography>} />
+        <CardMedia component="img" height="194" image={coverPhoto.url} alt={slug} />
+        <CardContent >
+            <Typography component="h3" variant='h6' color='text.primary' fontWeight={600}  >
+                {title}
+            </Typography>
+        </CardContent>
+        <Divider variant='middle' sx={{margin: "10px"}} />
+        <CardActions>
+             {/* <Link to={`blog/${slug}`} style={{textDecoration:"none", width:"100%" }}>  */}
+                <Button variant="outlined" size='small' sx={{width:"100%", borderRadius: 3 }} >مطالعه مقاله</Button>
+            {/* </Link> */}
+        </CardActions>
+    </Card>
   )
 }
 
 export default CardEl
+
