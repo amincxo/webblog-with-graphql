661ebe6121e46c814450ee6b8a51fa1fc5b654b4
unknown <amincxo@gmail.com>
2024-10-11 02:42:38 +0330

    add data to author page

--- src/components/author/Authors.js
+++ src/components/author/Authors.js
@@ -1,6 +1,17 @@
 import React from 'react'
 
+import { useQuery } from '@apollo/client'
+import { GET_AUTHORS_INFO } from '../../graphql/queries'
+
+
 function Authors() {
+    const {loading , data , error} = useQuery(GET_AUTHORS_INFO);
+
+    if(loading) return <h3>درحال بارگزاری نویسندگان</h3>
+
+    if(error) return <h3> متاسفانه زمان گرفتن اطلاعات به مشکل خوردیم </h3>
+
+    console.log(data)
   return (
     <div>Authors</div>
   )
