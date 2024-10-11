c20e83cb494bd031b68444ddb98e3f289e4708cb
unknown <amincxo@gmail.com>
2024-10-11 18:05:46 +0330

    add get autor query

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -35,4 +35,27 @@ const GET_AUTHORS_INFO = gql`
     }
 `;
 
-export {GET_BLOGS_INFO , GET_AUTHORS_INFO};
+const GET_AUTHOR_INFO = gql`
+    query getAuthorInfo($slug:String!){
+        author(where:{slug: $slug }){
+            avatar{
+        url
+        }
+        field
+        name
+        description{
+        html
+        }
+        post {
+        coverPhoto {
+            url
+        }
+        id
+        slug
+        title
+        }
+    }
+    }
+`
+
+export {GET_BLOGS_INFO , GET_AUTHORS_INFO ,GET_AUTHOR_INFO};
