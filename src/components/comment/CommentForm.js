e1af9a80f89effd49514afff2c53176365433bc1
unknown <amincxo@gmail.com>
2024-10-12 00:36:14 +0330

    complate comment form

--- src/components/comment/CommentForm.js
+++ src/components/comment/CommentForm.js
@@ -1,11 +1,40 @@
 import React ,{useState} from 'react'
 import { Button, Grid, TextField, Typography } from '@mui/material'
-
+import { useMutation } from '@apollo/client';
+import { SEND_COMMENT } from '../../graphql/mutation';
+import { ToastContainer , toast } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css"
 
 function CommentForm({slug}) {
     const [name , setName] = useState("");
     const [email , setEmail] = useState("");
     const [text , setText] = useState("");
+
+    const [sendComment , {loading , data , error}] = useMutation(SEND_COMMENT , {
+        variables:{
+            name,
+            email ,
+            text ,
+            slug,
+        }
+    })
+
+    const sendHandler = () => {
+        if (name && email && text ) {
+            sendComment()
+            
+        } else {
+            toast.warn("لطفا تمام فیلد ها رو پر کنید", {
+                position:"top-center"
+            });
+        }
+    }
+    if(data) {
+        toast.success("نظر ارسال شد منتظر تایید مدیر باشید", {
+            position:"top-center"
+        })
+};
+
   return (
     <Grid container sx={{boxShadow:"rgba(0,0,0,0.1)" , borderRadius:4, py:1, mt:5 }} >
         <Grid item xs={12} m={2}  >
@@ -23,8 +52,9 @@ function CommentForm({slug}) {
             <TextField label="متن کامنت " variant='outlined'  sx={{width:"100%", }} multiline minRows={4}  value={text} onChange={(e)=>setText(e.target.value)} />
         </Grid>
         <Grid item xs={12} m={2} >
-            <Button variant='contained' >ارسال</Button>
+            {loading ? <Button variant='contained' disabled > در حال ارسال </Button> : <Button variant='contained' onClick={sendHandler}  >ارسال</Button> }
         </Grid>
+        <ToastContainer />
     </Grid>
 )
 }
