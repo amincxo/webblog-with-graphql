0a40704c6dec05645a34ec0d957d660be54128ad
unknown <amincxo@gmail.com>
2024-10-11 18:31:53 +0330

    complate author page

--- src/components/author/AuthorPage.js
+++ src/components/author/AuthorPage.js
@@ -2,6 +2,7 @@ import { useQuery } from '@apollo/client';
 import React from 'react'
 import { useParams } from 'react-router-dom'
 import { GET_AUTHOR_INFO } from '../../graphql/queries';
+import { Avatar, Container, Grid, Typography } from '@mui/material';
 
 function AuthorPage() {
     const {slug} = useParams();
@@ -16,7 +17,20 @@ function AuthorPage() {
     console.log(data)
 
   return (
-    <div></div>
+    <Container maxWidth="lg" >
+        <Grid container mt={10}  >
+            <Grid item xs={12} display="flex" flexDirection="column" alignItems="center"  >
+                <Avatar src={data.author.avatar.url} sx={{width:250 , height:250}} />
+                <Typography component="h3" variant='h5' fontWeight={700} mt={4} >{data.author.name} </Typography>
+                <Typography component="p" variant='h5' color='text.secondary' mt={2}  >
+                    {data.author.field}
+                </Typography>
+            </Grid>
+            <Grid item xs={12} >
+                {data.author.description.html}
+            </Grid>
+        </Grid>
+    </Container>
   )
 }
 
