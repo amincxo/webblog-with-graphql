e0870eca289aa3c3515264c55c8156f8f6d26b39
unknown <amincxo@gmail.com>
2024-10-11 18:06:14 +0330

    geting data in authorpage

--- src/components/author/AuthorPage.js
+++ src/components/author/AuthorPage.js
@@ -1,8 +1,22 @@
+import { useQuery } from '@apollo/client';
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import { GET_AUTHOR_INFO } from '../../graphql/queries';
 
 function AuthorPage() {
+    const {slug} = useParams();
+    
+    const {loading, data , error} = useQuery(GET_AUTHOR_INFO, {
+        variables:{
+            slug
+        }
+    })
+    if(loading) return<h3>در حال بارگزاری</h3>;
+    if (error) return <h3>متاسفانه زمان بارگزاری دیتا به مشکل برخورد کردیم</h3>
+    console.log(data)
+
   return (
-    <div>AuthorPage</div>
+    <div></div>
   )
 }
 
